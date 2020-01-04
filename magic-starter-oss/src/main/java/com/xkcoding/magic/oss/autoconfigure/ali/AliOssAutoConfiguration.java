8be8ea89b726e26dc44415232daf15183b0e62f5
Yangkai.Shen <237497819@qq.com>
2020-01-04 14:04:01 +0800

    :recycle: 增加阿里 OSS Endpoint 协议

--- magic-starter-oss/src/main/java/com/xkcoding/magic/oss/autoconfigure/ali/AliOssAutoConfiguration.java
+++ magic-starter-oss/src/main/java/com/xkcoding/magic/oss/autoconfigure/ali/AliOssAutoConfiguration.java
@@ -16,10 +16,10 @@
 
 package com.xkcoding.magic.oss.autoconfigure.ali;
 
-import com.aliyun.oss.ClientConfiguration;
+import com.aliyun.oss.ClientBuilderConfiguration;
 import com.aliyun.oss.OSSClient;
-import com.aliyun.oss.common.auth.CredentialsProvider;
-import com.aliyun.oss.common.auth.DefaultCredentialProvider;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.common.comm.Protocol;
 import com.xkcoding.magic.oss.autoconfigure.OssAutoConfiguration;
 import com.xkcoding.magic.oss.autoconfigure.OssProperties;
 import com.xkcoding.magic.oss.support.ali.AliOssTemplate;
@@ -52,24 +52,27 @@ public class AliOssAutoConfiguration {
 	@ConditionalOnMissingBean
 	public OSSClient ossClient() {
 		// 创建ClientConfiguration。ClientConfiguration是OSSClient的配置类，可配置代理、连接超时、最大连接数等参数。
-		ClientConfiguration conf = new ClientConfiguration();
+		ClientBuilderConfiguration config = new ClientBuilderConfiguration();
 		// 设置OSSClient允许打开的最大HTTP连接数，默认为1024个。
-		conf.setMaxConnections(1024);
+		config.setMaxConnections(1024);
 		// 设置Socket层传输数据的超时时间，默认为50000毫秒。
-		conf.setSocketTimeout(50000);
+		config.setSocketTimeout(50000);
 		// 设置建立连接的超时时间，默认为50000毫秒。
-		conf.setConnectionTimeout(50000);
+		config.setConnectionTimeout(50000);
 		// 设置从连接池中获取连接的超时时间（单位：毫秒），默认不超时。
-		conf.setConnectionRequestTimeout(1000);
+		config.setConnectionRequestTimeout(1000);
 		// 设置连接空闲超时时间。超时则关闭连接，默认为60000毫秒。
-		conf.setIdleConnectionTime(60000);
+		config.setIdleConnectionTime(60000);
 		// 设置失败请求重试次数，默认为3次。
-		conf.setMaxErrorRetry(5);
-		CredentialsProvider credentialsProvider = new DefaultCredentialProvider(ossProperties.getAliOss()
+		config.setMaxErrorRetry(5);
+		// 配置协议
+		config.setProtocol(ossProperties.getAliOss()
+			.getHttps() ? Protocol.HTTPS : Protocol.HTTP);
+
+		return (OSSClient) new OSSClientBuilder().build(ossProperties.getAliOss()
+			.getEndpoint(), ossProperties.getAliOss()
 			.getAccessKey(), ossProperties.getAliOss()
-			.getSecretKey());
-		return new OSSClient(ossProperties.getAliOss()
-			.getEndpoint(), credentialsProvider, conf);
+			.getSecretKey(), config);
 	}
 
 	@Bean
